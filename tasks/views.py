d6892d9d34c9862f0bdac684162979e6b23eb38e
Keerthanagowda8 <gowdakeerthana814@gmail.com>
2025-11-30 19:56:30 +0530

    add cores-support

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import json
+from django.http import JsonResponse
 
-# Create your views here.
+def analyze_tasks(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        return JsonResponse(data, safe=False)
+    return JsonResponse({'error': 'Only POST allowed'}, status=400)
